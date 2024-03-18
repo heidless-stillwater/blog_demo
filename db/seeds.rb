dd08abbae15770bc405f21e250c4f3e59045bd72
heidless <rob.lockhart@yahoo.co.uk>
2024-03-18 11:50:14 +0000

    adding Posts

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,27 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+# 10.times do |x|
+#   Post.create(title: "Title", body: "Body words go here...")
+# end
+
+10.times do |n|
+  title  = "title 1 - #{n} - minimum length"
+  body = "body here - #{n} - minimum length"
+  Post.create!(
+    title: title,
+    body: body)
+end
+
+# 99.times do |n|
+#   name  = Faker::Name.name
+#   email = "example-#{n+1}@railstutorial.org"
+#   password = "password"
+#   User.create!(name: name,
+#               email: email,
+#               password:              password,
+#               password_confirmation: password,
+#               activated: true,
+#               activated_at: Time.zone.now)
+# end
